be7d0c6422939acc486c353b84e9f08c31133b2f
aleksa <zabumaphu@gmail.com>
2023-04-23 23:08:42 -0400

    Splash screen done. Icon changes started.

---
+++ app/src/main/java/com/example/sekutorakademijainfo/SplashScreen.java
@@ -0,0 +1,28 @@
+package com.example.sekutorakademijainfo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+
+            public void run() {
+                startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                finish();
+            }
+        }, 2500);
+    }
+}
