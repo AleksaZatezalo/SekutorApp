059778a32ad18a92d6c8bc16d67d57229a072660
aleksa <zabumaphu@gmail.com>
2023-06-19 20:02:21 -0400

    Registration finished.

--- app/src/main/java/com/example/sekutorakademijainfo/Register.java
+++ app/src/main/java/com/example/sekutorakademijainfo/Register.java
@@ -1,7 +1,9 @@
 package com.example.sekutorakademijainfo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -9,19 +11,23 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Register extends AppCompatActivity {
 
     EditText editTextEmail, editTextName, editTextPassword;
     Button buttonReg;
-
+    FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         editTextEmail = findViewById(R.id.editTextTextEmailAddress);
-        editTextName = findViewById(R.id.editTextName);
         editTextPassword = findViewById(R.id.editTextTextPassword);
         buttonReg = findViewById(R.id.Register);
 
@@ -32,6 +38,7 @@ public class Register extends AppCompatActivity {
                 email = String.valueOf(editTextEmail.getText());
                 name = String.valueOf(editTextName.getText());
                 password  = String.valueOf(editTextPassword.getText());
+                mAuth = FirebaseAuth.getInstance();
 
                 if (TextUtils.isEmpty(email)){
                     Toast.makeText(Register.this, "Enter email", Toast.LENGTH_SHORT);
@@ -45,6 +52,24 @@ public class Register extends AppCompatActivity {
                     Toast.makeText(Register.this, "Enter password", Toast.LENGTH_SHORT);
                     return;
                 }
+
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener( new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(Register.this, "Account created.",
+                                            Toast.LENGTH_SHORT).show();
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(Register.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                startActivity(intent);
+                                finish();
+                            }
+                        });
             }
 
         }));
