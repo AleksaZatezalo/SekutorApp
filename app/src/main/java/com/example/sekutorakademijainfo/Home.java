c1acf6e0848f6c519b6a80220c64b37b1f5b3abf
aleksa <zabumaphu@gmail.com>
2023-09-01 21:55:32 -0400

    WebView Finished

--- app/src/main/java/com/example/sekutorakademijainfo/Home.java
+++ app/src/main/java/com/example/sekutorakademijainfo/Home.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
@@ -34,8 +35,34 @@ public class Home extends AppCompatActivity {
         auth = FirebaseAuth.getInstance();
         user = auth.getCurrentUser();
         webView = (WebView) findViewById(R.id.webView);
-        webView.setWebViewClient(new WebViewClient());
-        webView.loadUrl("https://www.mmabeograd.org.rs/");
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        webView.getSettings().setSavePassword(true);
+        webView.getSettings().setSupportZoom(true);
+        webView.getSettings().setSaveFormData(true);
+        webView.getSettings().setSupportZoom(false);
+        webView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setSupportMultipleWindows(false);
+        webView.getSettings().setLightTouchEnabled(true);
+        webView.getSettings().setBuiltInZoomControls(true);
+        webView.getSettings().setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NORMAL);
+
+        webView.setWebViewClient(new WebViewClient(){
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                webView.loadUrl("javascript:(function() { " +
+                        "document.getElementsByClassName('mobile-nav-btn')[0].style.display='none'; " +
+                        "document.getElementsByClassName('navigation')[0].style.display='none'; " +
+                        "document.getElementsByClassName('navigation-info')[0].style.display='none'; "
+
+
+                );
+            }
+        });
+        webView.loadUrl("https://www.mmabeograd.org.rs");
 
         if (user == null){
             Intent intent = new Intent(getApplicationContext(), MainActivity.class);
