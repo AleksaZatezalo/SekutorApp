4aae8eb2e3bc9bafa616b343dc91844d26a6aeaa
aleksa <zabumaphu@gmail.com>
2023-09-03 16:14:26 -0400

    Filtering functionality added in the back end.

--- app/src/main/java/com/example/sekutorakademijainfo/Adapter.java
+++ app/src/main/java/com/example/sekutorakademijainfo/Adapter.java
@@ -4,21 +4,25 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
+public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> implements Filterable {
 
     private LayoutInflater layoutInflater;
     private List<String> data;
-
+    private List<String> dataFull;
     Adapter(Context context, List<String> data){
         this.layoutInflater = LayoutInflater.from(context);
         this.data = data;
+        this.dataFull = new ArrayList<>(data);
     }
     @NonNull
     @Override
@@ -41,10 +45,47 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
 
     public class ViewHolder extends RecyclerView.ViewHolder{
 
-        TextView textTitle, textTime;
+        TextView textTitle;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             textTitle = itemView.findViewById(R.id.Title);
         }
     }
+
+    @Override
+    public Filter getFilter(){
+        return classFilter;
+    }
+
+    private Filter classFilter = new Filter(){
+
+        @Override
+        protected FilterResults performFiltering(CharSequence charSequence) {
+            List<String> filteredList = new ArrayList<>();
+
+            if (charSequence == null || charSequence.length() == 0){
+                filteredList.addAll(dataFull);
+            } else {
+                String filterPattern = charSequence.toString().toLowerCase().trim();
+
+                for (String item : dataFull ) {
+                    if (item.toLowerCase().contains(charSequence)) {
+                        filteredList.add(item);
+                    }
+                }
+            }
+
+            FilterResults results = new FilterResults();
+            results.values = filteredList;
+
+            return results;
+        }
+
+        @Override
+        protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+            data.clear();
+            data.addAll((List) filterResults);
+            notifyDataSetChanged();
+        }
+    };
 }
