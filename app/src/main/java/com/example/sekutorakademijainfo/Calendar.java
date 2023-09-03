aa428740dbff11dbe63aa4c678ac65821b6a6098
aleksa <zabumaphu@gmail.com>
2023-09-03 19:17:38 -0400

    Class schedule updated.

--- app/src/main/java/com/example/sekutorakademijainfo/Calendar.java
+++ app/src/main/java/com/example/sekutorakademijainfo/Calendar.java
@@ -77,30 +77,6 @@ public class Calendar extends AppCompatActivity {
         classes.add("BJJ Za Decu (3-6) - Ponedeljak 18:30");
         classes.add("BJJ Za Decu (7-13) - Ponedeljak 19:30");
         classes.add("BJJ Za Odrasle- Ponedeljak 20:45");
-
-
-        classes.add("MMA Rvanje - Utorak 9:30");
-        classes.add("Grappling - Utorak 18:00");
-        classes.add("MMA - Utorak 20:45");
-
-        classes.add("Grappling - Sreda 7:30");
-        classes.add("MMA Stand Up - Sreda 9:30");
-        classes.add("BJJ Za Decu (3-6) - Sreda 18:30");
-        classes.add("BJJ Za Odrasle - Sreda 20:45");
-
-        classes.add("MMA Rvanje - Cetvrtak 9:30");
-        classes.add("Grappling -  Cetvrtak 18:00");
-        classes.add("MMA- Cetvrtak 20:45");
-
-        classes.add("Grappling - Petak 7:30");
-        classes.add("MMA Stand Up - Petak 18:00");
-        classes.add("BJJ Za Decu (3-6) - Petak 18:30");
-        classes.add("BJJ Za Odrasle - Petak 20:45");
-
-        classes.add("MMA Rvanje - Subota 12:00");
-        classes.add("Grappiling - Subota 13:30");
-
-        // Sala 2 Classes
         classes.add("Cardioboks za Zene - Ponedeljak 17:30");
         classes.add("Kickboks - Ponedeljak 18:30");
         classes.add("BJJ Za Decu (3-6) - Ponedeljak 18:30");
@@ -110,26 +86,44 @@ public class Calendar extends AppCompatActivity {
         classes.add("Boks za Odrasle - Utorak 18:30");
         classes.add("Funcialnost za Zene - Utorak 18:30");
         classes.add("BJJ Fundamentals - Utorak 20:45");
+        classes.add("MMA Rvanje - Utorak 9:30");
+        classes.add("Grappling - Utorak 18:00");
+        classes.add("MMA - Utorak 20:45");
 
+        classes.add("Grappling - Sreda 7:30");
+        classes.add("MMA Stand Up - Sreda 9:30");
+        classes.add("BJJ Za Decu (3-6) - Sreda 18:30");
+        classes.add("BJJ Za Odrasle - Sreda 20:45");
         classes.add("Cardioboks za Zene - Sreda 17:30");
         classes.add("Kickboks - Sreda 18:30");
         classes.add("Boks Za Decu - Sreda 17:30");
         classes.add("MMA Basic - Sreda 20:30");
 
+
+        classes.add("MMA Rvanje - Cetvrtak 9:30");
+        classes.add("Grappling -  Cetvrtak 18:00");
+        classes.add("MMA- Cetvrtak 20:45");
         classes.add("Kondicional Trening - Cetvrtak 17:30");
         classes.add("Boks - Cetvrtak 18:30");
         classes.add("Funcionalnost Za Zene - Cetvrtak 18:30");
         classes.add("BJJ Fundamentals - Cetvrtak 20:45");
 
+        classes.add("Grappling - Petak 7:30");
+        classes.add("MMA Stand Up - Petak 18:00");
+        classes.add("BJJ Za Decu (3-6) - Petak 18:30");
+        classes.add("BJJ Za Odrasle - Petak 20:45");
         classes.add("Cardio Kickboks - Petak 17:30");
         classes.add("Kickboks - Petak 18:30");
         classes.add("BJJ Za Decu (3-6) - Petak 19:30");
         classes.add("MMA Basic - Petak 20:35");
 
+        classes.add("MMA Rvanje - Subota 12:00");
+        classes.add("Grappiling - Subota 13:30");
         classes.add("Funcionalnost Za Zene- Subota 11:00");
         classes.add("BJJ Fundamentals - Subota 12:00");
         classes.add("Boks Za Odrasle - Subota 12:30");
         classes.add("Kondicional Trening - Subota 14:35");
+
         schedule.setLayoutManager(new LinearLayoutManager((this)));
         cardAdapter = new Adapter(this, classes);
         schedule.setAdapter(cardAdapter);
